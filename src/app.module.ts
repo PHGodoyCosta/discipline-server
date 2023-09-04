ad3926020d0c07f5b14dd25b5f4260e23467b3be
Pablo Pereira <pablopereira27@gmail.com>
2023-09-04 16:18:40 -0300

    Inclusão do TypeORM no projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'XXXXXX',
+      password: 'XXXXXX',
+      database: 'discipline_db',
+      entities: ["dist/**/*.entity.js"],
+
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
