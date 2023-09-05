3918958b6b928eadfce88aa92bbe120759f1203c
Pablo Pereira <pablopereira27@gmail.com>
2023-09-04 23:21:45 -0300

    Adicionando NestJS Config para inserir os dados de acesso ao banco de dados.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+
 import { AvaliationsModule } from './avaliations/avaliations.module';
 
 import { AppController } from './app.controller';
@@ -8,16 +11,22 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'XXXXXX',
-      password: 'XXXXXX',
-      database: 'discipline_db',
-      entities: ["dist/**/*.entity.js"],
-
-      synchronize: true,
+    ConfigModule.forRoot({
+      load: [databaseConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: 'discipline_db',
+        entities: ["dist/**/*.entity.js"],
+        synchronize: configService.get('database.synchronize'),
+      }),
     }),
 
     AvaliationsModule,
---
+++ src/config/database.config.ts
@@ -0,0 +1,16 @@
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+
+export default () => {
+    var databaseOptions: MysqlConnectionOptions = {
+        type: "mysql",
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: parseInt(process.env.DATABASE_PORT) || 3306,
+        username: process.env.DATABASE_USERNAME || 'root',
+        password: process.env.DATABASE_PASSWORD || '',
+        synchronize: (process.env.DATABASE_SYNC == "true")
+    }
+
+    return {
+        database: databaseOptions
+    }
+};
