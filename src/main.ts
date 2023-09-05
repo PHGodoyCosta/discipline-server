3e7374c39b976696c5832bf897071acb4bc4e3c5
Pablo Pereira <pablopereira27@gmail.com>
2023-09-05 15:49:58 -0300

    FIX - Habilitando CORS para todas as origens.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  var corsConfig = {
-    origin: ['*'],
+  var corsConfig: CorsOptions = {
+    origin: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     preflightContinue: false,
     optionsSuccessStatus: 204,
