adfe92f0526485da10aa52b6a103f34cb9a30104
Pablo Pereira <pablopereira27@gmail.com>
2023-09-05 00:08:14 -0300

    Configuração da porta de acesso ao sistema

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, () => {
+    const apiUrl = process.env.API_URL || `http://localhost:${process.env.PORT || '3000'}`
+    console.log('|---------------------------------------------------------|')
+    console.log('|------------------- Discipline Server -------------------|')
+    console.log('|---------------------------------------------------------|')
+    console.log(`Server listening -> ${apiUrl}`);
+    // console.log(`Swagger Documentation -> ${apiUrl}/docs`);
+  });
 }
 bootstrap();
