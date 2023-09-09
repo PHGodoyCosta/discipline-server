014c924d4e99918658187295413d7f1d2acb4a30
Pablo Pereira <pablopereira27@gmail.com>
2023-09-09 09:26:45 -0300

    Criação de DTO para ajustar o retorno do objeto Instituição

---
+++ src/institutions/dto/response-institution.dto.ts
@@ -0,0 +1,26 @@
+import { Exclude,Expose } from 'class-transformer';
+import { IsNumber, IsString } from 'class-validator';
+
+import { Institution } from '../entities/institution.entity';
+
+export class ResponseInstitutionDTO {
+    @Exclude()
+    @IsNumber()
+    id: number;
+
+    @IsString()
+    name: string;
+
+    @IsString()
+    hash: string;
+
+    @Expose()
+    @IsString()
+    get icon(): string {
+        return `${process.env.API_URL}/uploads/institutions/${this.hash}.png`;
+    }
+
+    constructor(partial: Partial<Institution>) {
+        Object.assign(this, partial);
+    }
+}
--- src/institutions/institutions.controller.ts
+++ src/institutions/institutions.controller.ts
@@ -1,18 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  ClassSerializerInterceptor,
+  UseInterceptors
+} from '@nestjs/common';
 
 import { InstitutionsService } from './institutions.service';
 
 import { Institution } from './entities/institution.entity';
 import { CreateInstitutionDto } from './dto/create-institution.dto';
 import { UpdateInstitutionDto } from './dto/update-institution.dto';
+import { ResponseInstitutionDTO } from './dto/response-institution.dto';
 
 @Controller('institutions')
+@UseInterceptors(ClassSerializerInterceptor)
 export class InstitutionsController {
   constructor(private readonly institutionService: InstitutionsService) { }
 
   @Get()
-  findAll(): Promise<Institution[]> {
-    return this.institutionService.findAll();
+  findAll(): Promise<ResponseInstitutionDTO[]> {
+    return this.institutionService.findAll()
+      .then(res => res.map(item => new ResponseInstitutionDTO(item)));
   }
 
   @Get(':hash')
